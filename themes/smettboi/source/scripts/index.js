550790e96e542fed48674a1b050a7c7094849d6e
Zane Adickes <zaneadix@gmail.com>
2018-01-29 08:47:22 -0500

    more experimenting

--- themes/smettboi/source/scripts/index.js
+++ themes/smettboi/source/scripts/index.js
@@ -1,70 +1,107 @@
 
-// cause I didnt want to write it https://codepen.io/chrissp26/pen/gBrdo?editors=0110
-
-var stickyHeaders = (function() {
-  
-  var $window = $(window),
-      $stickies;
-
-  var load = function(stickies) {
-
-    if (typeof stickies === "object" && stickies instanceof jQuery && stickies.length > 0) {
-
-      $stickies = stickies.each(function() {
-
-        var $thisSticky = $(this).wrap('<div class="followWrap" />');
-  
-        $thisSticky
-            .data('originalPosition', $thisSticky.offset().top)
-            .data('originalHeight', $thisSticky.outerHeight())
-              .parent()
-              .height($thisSticky.outerHeight());             
-      });
-
-      $window.off("scroll.stickies").on("scroll.stickies", function() {
-          _whenScrolling();     
-      });
-    }
-  };
-
-  var _whenScrolling = function() {
-
-    $stickies.each(function(i) {            
-
-      var $thisSticky = $(this),
-          $stickyPosition = $thisSticky.data('originalPosition');
-
-      if ($stickyPosition <= $window.scrollTop()) {        
-        
-        var $nextSticky = $stickies.eq(i + 1),
-            $nextStickyPosition = $nextSticky.data('originalPosition') - $thisSticky.data('originalHeight');
-
-        $thisSticky.addClass("fixed");
-
-        if ($nextSticky.length > 0 && $thisSticky.offset().top >= $nextStickyPosition) {
-
-          $thisSticky.addClass("absolute").css("top", $nextStickyPosition);
-        }
-
-      } else {
-        
-        var $prevSticky = $stickies.eq(i - 1);
-
-        $thisSticky.removeClass("fixed");
-
-        if ($prevSticky.length > 0 && $window.scrollTop() <= $thisSticky.data('originalPosition') - $thisSticky.data('originalHeight')) {
-
-          $prevSticky.removeClass("absolute").removeAttr("style");
-        }
-      }
-    });
-  };
-
-  return {
-    load: load
-  };
-})();
-
 $(function() {
-  stickyHeaders.load($(".followMeBar"));
+
+    var $window = $(window);
+    var $header = $('header.navbar');
+    var $postHeaders = $('.post-header');
+    var $ratings = $('.rating');
+
+    function initRatings () {
+        $ratings.each(function (index) {
+            $rating = $(this);
+            $fill = $rating.find('.fill');
+            $fill.attr('style', 'width: 0%');
+        })
+    }
+
+    function initFixiePositions () {
+        $header.data('setHeight', $header.outerHeight());
+        $postHeaders.each(function () {
+            $postHeader = $(this);
+            var attrs = scrubFixie($postHeader);
+            setFixiePositions($postHeader);
+            $postHeader
+                .attr('class', attrs.clazz)
+                .attr('style', attrs.style);
+        });
+        fixieCheck();
+    }
+
+    function setFixiePositions (postHeader) {
+        var $postHeader = (postHeader instanceof jQuery) ? postHeader : $(postHeader);
+        $postHeader
+            .data('setTop', $postHeader.offset().top)
+            .data('setHeight', $postHeader.outerHeight())
+            .parent()
+            .height($postHeader.outerHeight());
+    }
+
+    function scrubFixie ($postHeader) {
+        var attrs = {};
+        attrs.clazz = $postHeader.attr('class');
+        attrs.style = $postHeader.attr('style');
+        $postHeader.removeClass('fixed absolute').removeAttr("style");
+        return attrs;
+    }
+    
+    function fixieCheck () {
+
+        var headerHeight = $header.data('setHeight');
+        var $fixedHeader;
+        var $absoluteHeader;
+
+        for (var index = 0; index <= $postHeaders.length; index++) {
+
+            var $postHeader = $postHeaders.eq(index);
+            var setTop = $postHeader.data('setTop');
+
+            if (setTop <= ($window.scrollTop() + headerHeight)) {
+                $fixedHeader = $postHeader;
+                $absoluteHeader = (index-1 >= 0) ? $postHeaders.eq(index-1) : null;
+            } else {
+                break;
+            }
+        }
+
+        
+        if ($fixedHeader) {
+            console.log('fixed:', $fixedHeader.find('.title').text());
+        }
+        if ($absoluteHeader) {
+            console.log('absolute:', $absoluteHeader.find('.title').text());
+        }
+        
+
+
+        // $postHeaders.each(function (index) {
+
+        //     var $postHeader = $(this);
+        //     var setTop = $postHeader.data('setTop');
+
+        //     if (setTop <= ($window.scrollTop() + headerHeight)) {
+        //         $postHeader.addClass("fixed").css("top", '0px');;
+
+
+        //         var $nextPostHeader = $postHeaders.eq(index+1);
+        //         var $nextTop = $nextPostHeader.data('setTop') - $postHeader.data('setHeight');
+
+        //         if ($nextPostHeader.length > 0 && $postHeader.offset().top >= $nextTop) {
+        //             $postHeader.addClass("absolute").css("top", $nextTop);
+        //         }
+        //     } else {
+
+        //         $postHeader.removeClass("fixed");
+        //         var $prevPostHeader = $postHeaders.eq(index - 1);
+        //         if ($prevPostHeader.length > 0 && $window.scrollTop() <= $postHeader.data('setTop') - $postHeader.data('setHeight')) {
+        //             $prevPostHeader.removeClass("absolute").removeAttr("style");
+        //         }
+        //     }
+        // });
+    }
+
+    $postHeaders.wrap('<div class="fixieHeightKeeper" />');
+    $window.on('resize', initFixiePositions);
+    $window.on("scroll", fixieCheck);
+    initFixiePositions();
+    // initRatings();
 });
